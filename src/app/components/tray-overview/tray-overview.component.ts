78b974cd1761587981e960fbfcb01d1159c1f54b
Andrey Epifancev <andrey.epifancev@ssi-schafer.ru>
2025-04-25 10:15:23 +0400

    add ean input

--- src/app/components/tray-overview/tray-overview.component.ts
+++ src/app/components/tray-overview/tray-overview.component.ts
@@ -8,4 +8,11 @@ import { Component, Input } from '@angular/core';
 export class TrayOverviewComponent {
   @Input() tray: { id: string; type: number; rowCount: number } | null = null;
   @Input() selectedSection: number | null = null;
+
+  eanCode: string = '';
+
+  submitEAN(): void {
+    console.log('EAN submitted:', this.eanCode);
+    // Добавьте необходимую логику для обработки EAN
+  }
 }
