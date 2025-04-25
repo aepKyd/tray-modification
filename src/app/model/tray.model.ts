fa734a87469572963a3b81f935e16021d9c95048
Andrey Epifancev <andrey.epifancev@ssi-schafer.ru>
2025-04-25 14:56:11 +0400

    add model for object tray

---
+++ src/app/model/tray.model.ts
@@ -0,0 +1,75 @@
+export class Tray {
+  id: string;
+  type: number;
+  activityCode?: string;
+  lines?: Line[];
+
+  constructor({
+    id,
+    type,
+    activityCode,
+    lines,
+  }: {
+    id: string;
+    type: number;
+    activityCode?: string;
+    lines?: Line[];
+  }) {
+    this.id = id;
+    this.type = type;
+    this.activityCode = activityCode;
+    this.lines = lines;
+  }
+}
+
+export class Line {
+  lineNumber: number;
+  productCode: string;
+  quantity: number;
+  reserveQuantity: number;
+  expiryDate: string;
+  lockCode: string;
+  trayCode: string;
+  acceptanceNumber: string;
+  sscc: string;
+  ean: string;
+  client: string;
+
+  constructor({
+    lineNumber,
+    productCode,
+    quantity,
+    reserveQuantity,
+    expiryDate,
+    lockCode,
+    trayCode,
+    acceptanceNumber,
+    sscc,
+    ean,
+    client,
+  }: {
+    lineNumber: number;
+    productCode: string;
+    quantity: number;
+    reserveQuantity: number;
+    expiryDate: string;
+    lockCode: string;
+    trayCode: string;
+    acceptanceNumber: string;
+    sscc: string;
+    ean: string;
+    client: string;
+  }) {
+    this.lineNumber = lineNumber;
+    this.productCode = productCode;
+    this.quantity = quantity;
+    this.reserveQuantity = reserveQuantity;
+    this.expiryDate = expiryDate;
+    this.lockCode = lockCode;
+    this.trayCode = trayCode;
+    this.acceptanceNumber = acceptanceNumber;
+    this.sscc = sscc;
+    this.ean = ean;
+    this.client = client;
+  }
+}
